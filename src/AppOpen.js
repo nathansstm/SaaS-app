0c06a43b4a86ba6d579d9962756f2ab15e16bffd
Nathaniel <nathans.stm@aol.com>
2024-10-28 00:45:56 +0000

    Added AppOpen.js

---
+++ src/AppOpen.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useSnackbar } from './layouts/components/SnackbarContext';
+import { useLocation } from 'react-router-dom';
+
+const AppOpen = ({ children }) => {
+  const showSnackbar = useSnackbar();
+  const location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname.split('/').pop(); // Get the last segment of the path
+
+    // Make a fetch request to your backend route
+    const sendTrace = async () => {
+      try {
+        const response = await fetch(`/api/span/${page}`, {
+          method: 'GET', // Change method to GET
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          showSnackbar('Span Pushed!');
+        } else {
+          throw new Error('Span Failed');
+        }
+      } catch (error) {
+        showSnackbar('Span Error!');
+      }
+    };
+
+    sendTrace();
+  }, [location.pathname]);
+
+  return <>{children}</>;
+};
+
+export default AppOpen;
