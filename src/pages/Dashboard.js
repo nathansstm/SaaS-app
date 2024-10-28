a5d669fecbe190284a40578e3f8d09e68fef9ab1
Nathaniel <nathans.stm@aol.com>
2024-10-28 14:01:00 +0000

    Updated Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { Container, Typography, Box } from '@mui/material';
 import CustomDynamicInputComponent from './components/CustomDynamicInputComponent';
 import CustomPipelineComponent from './components/CustomPipelineComponent';
-import CustomDataGridComponent from './components/CustomDataGridComponent'; // Import CustomDataGridComponent
-import { CustomRecordsProvider } from './components/CustomRecordsContext'; // Import the CustomRecordsProvider
+import CustomDataGridComponent from './components/CustomDataGridComponent';
+import CustomSpanComponent from './components/CustomSpanComponent';
+import { CustomRecordsProvider } from './components/CustomRecordsContext';
 
 function Dashboard() {
   return (
@@ -16,20 +17,25 @@ function Dashboard() {
 
         {/* Box for stacking components, allowing them to push each other down */}
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+  
+          {/* CustomSpanComponent graph */}
+          <Box sx={{ width: '100%' }}>
+            <CustomSpanComponent />
+          </Box>
           
           {/* CustomDynamicInputComponent grows naturally without restriction */}
           <Box sx={{ width: '100%', paddingBottom: 2 }}>
             <CustomDynamicInputComponent />
           </Box>
           
-          {/* CustomPipelineComponent naturally pushed down by the growing inputs */}
-          <Box sx={{ width: '100%' }}>
-            <CustomPipelineComponent />
-          </Box>
-
-          {/* Add the CustomDataGridComponent here */}
+          {/* CustomDataGridComponent naturally pushed down by the growing inputs */}
           <Box sx={{ width: '100%' }}>
             <CustomDataGridComponent />
+          </Box>
+
+          {/* Add the CustomPipelineComponent here */}
+          <Box sx={{ width: '100%' }}>
+            <CustomPipelineComponent />
           </Box>
         </Box>
       </Container>
@@ -38,5 +44,6 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
 
 
